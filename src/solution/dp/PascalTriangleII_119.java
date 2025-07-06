bd4ff915a998e56729837c7e68f8f04d354effa5
fanfan <fanwuyina@163.com>
2025-07-07 00:16:33 +0800

    dp_day_01

---
+++ src/solution/dp/PascalTriangleII_119.java
@@ -0,0 +1,32 @@
+package solution.dp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PascalTriangleII_119 {
+
+
+    public List<Integer> getRow(int rowIndex) {
+        if(rowIndex == 0){
+            return Arrays.asList(1);
+        }
+        if(rowIndex == 1){
+            return Arrays.asList(1,1);
+        }
+        List<Integer> preRow = getRow(rowIndex - 1);
+
+        List<Integer> result = new ArrayList<>();
+
+        result.add(1);
+        int j = 1;
+        while (j < rowIndex){
+            result.add(preRow.get(j-1) + preRow.get(j));
+            j++;
+        }
+        result.add(1);
+
+        return result;
+
+    }
+}
---
+++ src/solution/dp/PascalTriangle_118.java
@@ -0,0 +1,33 @@
+package solution.dp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle_118 {
+
+    public List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        //while j = 0 || i = j then f(i)(j) = 1
+        //else f(i)(j) = f(i-1)f(j-1) + f(i-1)f(j)
+        List<Integer> preRowList = new ArrayList<>();
+        int i = 0;
+        while (i < numRows){
+            int j = 0;
+            List<Integer> currentRowList = new ArrayList<>();
+            while (j <= i){
+                if (j == 0 || i == j) {
+                    currentRowList.add(1);
+                }else{
+                    currentRowList.add(preRowList.get(j-1) + preRowList.get(j));
+                }
+                j++;
+            }
+            i++;
+            preRowList = currentRowList;
+            result.add(currentRowList);
+        }
+        return result;
+    }
+}
